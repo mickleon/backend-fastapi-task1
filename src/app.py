09d19259d2f3da59a124088100ca2e8d50db8daa
mickleon <mickleon64@gmail.com>
2026-03-06 09:43:52 +0400

    chore: add tags to routers

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from src.api.base.base import base_router
+from src.api.base.user import user_router
+from src.api.base.category import category_router
+from src.api.base.location import location_router
+from src.api.base.post import post_router
+from src.api.base.comment import comment_router
 
 
 def create_app() -> FastAPI:
@@ -14,7 +18,11 @@ def create_app() -> FastAPI:
         allow_headers=['*'],
     )
 
-    app.include_router(base_router, prefix='/base', tags=['Base APIs'])
+    app.include_router(user_router, prefix='/user', tags=['User CRUD'])
+    app.include_router(category_router, prefix='/category', tags=['Category CRUD'])
+    app.include_router(location_router, prefix='/location', tags=['Location CRUD'])
+    app.include_router(post_router, prefix='/post', tags=['Post CRUD'])
+    app.include_router(comment_router, prefix='/comment', tags=['Comment CRUD'])
 
     return app
 
